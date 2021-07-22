19edbaa408ad962298f13dbb5cd665b8341d9d3b
agustin <agustin.j@zohomail.com>
2021-07-22 07:28:15 -0300

    Add minimum count value validation
    fixes #1

--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 
 
 class Event(models.Model):
@@ -16,4 +18,12 @@ class Event(models.Model):
         null=False,
         default=1,
         verbose_name='Count',
+        validators=[
+            MinValueValidator(1),
+        ],
     )
+
+    def save(self, *args, **kwargs):
+        if self.count == 0:
+            raise ValidationError('Count minimum value is 1')
+        super(Event, self).save(*args, **kwargs)
--- src/core/tests/test_models.py
+++ src/core/tests/test_models.py
@@ -1,5 +1,6 @@
 import pytest
 from datetime import date
+from django.core.exceptions import ValidationError
 
 from core.models import Event
 
@@ -8,6 +9,7 @@ from core.models import Event
 class TestsEvents:
     """Test suite for Events model
     """
+
     def test_creating_event_successfully(self):
         """Test creating an event without raising error
         """
@@ -19,3 +21,12 @@ class TestsEvents:
         assert isinstance(e.event, str)
         assert isinstance(e.date, date)
         assert e.count
+
+    def test_creating_an_event_with_zero_count_raises_error(self):
+        """Test creating an event with 0 count raises error
+        """
+        with pytest.raises(ValidationError):
+            e = Event.objects.create(
+                event='some',
+                count=0,
+            )
