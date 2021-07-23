10df9d95d36eab0b1d0b22004b1344cb0f7e747c
agustin <agustin.j@zohomail.com>
2021-07-23 17:09:30 -0300

    Adding postgres and celery support

--- src/events/settings/base.py
+++ src/events/settings/base.py
@@ -112,3 +112,14 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ]
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': 5432,
+    }
+}
